3627255870714ee5e4471b8ce132d90f22a795e9
agiluzaki <118668141+agiluzaki@users.noreply.github.com>
2023-07-29 06:35:43 +0700

    Add files via upload

---
+++ Java/PEWARISAN/src/App.java
@@ -0,0 +1,72 @@
+//Class Abstarct
+//Inheritnace / pewarisan pada 
+//Encapsulation / memastikan
+//Overriding / definisi ulang
+//Overloading 
+
+abstract class Peminjaman {
+    void denda() {
+    }
+
+    void pengemablian() {
+    }
+}
+
+class PBO extends Peminjaman {
+    @Override
+    void denda() {
+
+    }
+}
+
+class Parent {
+    public int x;
+
+    void print(String tipeAnggota, int bayar) {
+        System.out.println("Print Parent");
+    }
+
+    void print(String tipeAnggota) {
+        System.out.println("Print Parent");
+    }
+
+    void print() {
+        System.out.println("Print 2");
+    }
+}
+
+class A extends Parent {
+
+    @Override
+    void print() {
+        System.out.println("Print A");
+
+    }
+
+    void test() {
+        print();
+        x = 5;
+    }
+}
+
+class B extends A {
+    void test2() {
+        print();
+    }
+}
+
+public class App extends Parent {
+
+    public static void main(String[] args) throws Exception {
+        Parent parent = new Parent();
+        parent.print();
+        parent.print("Mahasiswa");
+
+        A a = new A();
+        a.print();
+
+        //Peminjaman peminjaman = new Peminjaman();
+
+    }
+
+}
